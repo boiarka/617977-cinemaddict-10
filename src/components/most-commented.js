f8cfacc34448e1dc749e5dbe7373c55325eb4190
yurii <yurii.androsiuk@gmail.com>
2019-11-25 02:19:05 +0200

    add webpack, components

---
+++ src/components/most-commented.js
@@ -0,0 +1,7 @@
+export const mostCommentedTemplate = () => (
+  `<section class="films-list--extra" id="most-commented">
+	<h2 class="films-list__title">Most commented</h2>
+	<div class="films-list__container">
+	</div>
+</section>`
+);
---
+++ src/components/top-rated.js
@@ -0,0 +1,7 @@
+export const topRatedTemplate = () => (
+  `<section class="films-list--extra" id="top-rated">
+	<h2 class="films-list__title">Top rated</h2>
+	<div class="films-list__container">
+	</div>
+</section>`
+);
